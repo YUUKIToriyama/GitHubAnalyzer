fc69048146c7e226b1a0814e02aba0e59da3b584
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-04-28 22:18:07 +0900

    クラス化

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # GitHubAnalyzer
 Analyze someone's GitHub Activities
+
+## Usage
+```javascript
+const analyzer = new GitHubAnalyzer();
+const userName = "YUUKIToriyama";
+analyzer.evaluateLanguageFrequencyOfTheUser(userName).then(result => {
+	console.log(result);
+});
+```
--- main.js
+++ main.js
@@ -1,49 +1,48 @@
 const { Octokit } = require("@octokit/core");
 const settings = require("./settings");
-const octokit = new Octokit(settings);
 
-const getAllReposOfTheUser = (userName) => {
-	return octokit.request("GET /users/{username}/repos", {
-		username: userName,
-		sort: "updated",
-		per_page: 100,
-	}).then(response => {
-		return response.data.map(repository => repository.name);
-	}).catch(error => {
-		console.log(error);
-	});
-};
-
-const getLanguagesUsedInTheRepo = (userName, repositoryName) => {
-	return octokit.request("GET /repos/{owner}/{repo}/languages", {
-		owner: userName,
-		repo: repositoryName
-	}).then(response => {
-		return response.data;
-	})
-}
-
-//getUsersAllRepos("YUUKIToriyama").then(a => { console.log(a.join("\n")) })
-/*
-getLanguagesUsedInRepo("YUUKIToriyama", "imageCompare").then(result => {
-	console.log(result)
-});
-*/
-
-const userName = "YUUKIToriyama";
-getAllReposOfTheUser(userName).then(async repositoryNames => {
-	const frequencyOfLanguages = {};
-	for (let repositoryName of repositoryNames) {
-		await getLanguagesUsedInTheRepo(userName, repositoryName).then(frequency => {
-			Object.keys(frequency).forEach(language => {
-				if (!frequencyOfLanguages.hasOwnProperty(language)) {
-					frequencyOfLanguages[language] = 0;
-				}
-				frequencyOfLanguages[language] = + frequency[language];
-			});
-		});
+class GitHubAnalyzer {
+	constructor(_settings) {
+		this.settings = _settings || settings
+		this.octokit = new Octokit(this.settings)
 	}
-	return frequencyOfLanguages;
-}).then(result => {
-	console.log(result);
-})
+
+	getAllReposOfTheUser = (userName) => {
+		return this.octokit.request("GET /users/{username}/repos", {
+			username: userName,
+			sort: "updated",
+			per_page: 100,
+		}).then(response => {
+			return response.data.map(repository => repository.name);
+		}).catch(error => {
+			console.log(error);
+		})
+	}
+
+	getLanguagesUsedInTheRepo = (userName, repositoryName) => {
+		return this.octokit.request("GET /repos/{owner}/{repo}/languages", {
+			owner: userName,
+			repo: repositoryName
+		}).then(response => {
+			return response.data;
+		})
+	}
+
+	evaluateLanguageFrequencyOfTheUser = (userName) => {
+		return this.getAllReposOfTheUser(userName).then(async repositoryNames => {
+			const frequencyOfLanguages = {};
+			for (let repositoryName of repositoryNames) {
+				await this.getLanguagesUsedInTheRepo(userName, repositoryName).then(frequency => {
+					Object.keys(frequency).forEach(language => {
+						if (!frequencyOfLanguages.hasOwnProperty(language)) {
+							frequencyOfLanguages[language] = 0;
+						}
+						frequencyOfLanguages[language] = + frequency[language];
+					});
+				});
+			}
+			return frequencyOfLanguages;
+		})
+	}
+};
+module.exports = GitHubAnalyzer;
